14adaee355cc32646feeddde9470da71c8f26a28
Shilpa <knsl.sree@gmail.com>
2023-05-03 13:03:46 +0200

    Latest

--- frontend/main.js
+++ frontend/main.js
@@ -24,7 +24,37 @@ async function addBand(){
     document.getElementById('bandForm').addEventListener('submit',async(event)=>{
         //Prevent default behaviour of form
         event.preventDefault();
-    })
+
+        // get name and genre from input fields
+        const name= document.getElementById ('bandName').value ;
+        const genre = document.getElementById ('bandGenre').value;
+
+        //create object to send through POST request
+        const band= {
+            name: name,
+            genre: genre
+        };
+        console.log(band)
+
+    
+    // The POST request
+    const response = await fetch('http://localhost:3000/bands' , {
+        method : 'POST',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(band)
+        });
+
+        
+        //Convert response 
+    const result = await response.json();
+    //log result
+    console.log(result);
+    //show bands again
+    getAllBands();
+});
 }
 getAllBands();
+addBand();
 
